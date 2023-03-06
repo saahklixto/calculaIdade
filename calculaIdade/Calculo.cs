aa06d8fb17e676b98c10fec2b01efa8ba042a6a6
Samara <Samara@DESKTOP-R1UELAH>
2023-03-06 11:09:17 -0300

    Adicionar arquivos de projeto.

---
+++ calculaIdade/Calculo.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Calcula_idade
+
+
+
+{
+
+    public class Calculo
+
+    {
+
+
+
+        public int LeiaValorNumerico(string mensagemParaUsuario)
+
+        {
+
+
+
+            bool numeroEhValido = false;
+
+
+
+            int numeroInformadoPeloUsuario = 0;
+
+
+
+            while (!numeroEhValido)
+
+            {
+
+                Console.WriteLine(mensagemParaUsuario);
+
+
+
+                var valorInformadoPeloUsuario = Console.ReadLine();
+
+
+
+                numeroEhValido = int.TryParse(valorInformadoPeloUsuario, out numeroInformadoPeloUsuario);
+
+
+
+                if (!numeroEhValido)
+
+                {
+
+                    Console.WriteLine("Numero invalido");
+
+
+
+                }
+
+
+
+            }
+
+            return numeroInformadoPeloUsuario;
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
+
+
---
+++ calculaIdade/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Runtime.InteropServices;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+
+
+namespace Calcula_idade
+
+{
+
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+
+
+            Console.WriteLine("Escreva seu nome");
+
+            string nome = Console.ReadLine();
+
+
+
+
+
+            bool dataDeNascimentoEhValida = false;
+
+            DateTime dataDeNascimento = DateTime.Now;
+
+
+
+            while (!dataDeNascimentoEhValida)
+
+            {
+
+
+
+                
+
+
+
+                string Data = Console.ReadLine();
+                string[] strData = Data.Split('/');
+                Console.WriteLine(string.Format("dia : {0}", strData[0]));
+                Console.WriteLine(string.Format("Mes : {0}", strData[1]));
+                Console.WriteLine(String.Format("Ano: {0} ", strData[2]));
+
+                Console.ReadKey();
+                Calculo idade = new Calculo();
+
+                //Data = idade.LeiaValorNumerico(nome + " informe o seu dia de nascimento: ");
+
+
+
+
+                //mes = LeiaValorNumerico("Informe o seu mês de nascimento: "); 
+
+                //ano = LeiaValorNumerico("Informe o seu ano de nascimento: "); 
+
+             
+               
+                //estuda array de string 
+
+
+
+
+
+                try
+
+                {
+
+                    dataDeNascimento = new DateTime();
+
+                    dataDeNascimentoEhValida = true;
+
+                }
+
+
+
+                catch (Exception)
+
+                {
+
+                    Console.WriteLine("Data de nascimento inválida.");
+
+                }
+
+
+
+
+
+            }
+
+            var idadeDaPessoa = (int)(DateTime.Today.Subtract(dataDeNascimento).TotalDays / 365);
+
+            Console.WriteLine(nome + " tem " + idadeDaPessoa + " anos.");
+
+
+
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
